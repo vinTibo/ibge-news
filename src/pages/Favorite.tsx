2d5760bb94a32f57b88da5edfbfdddc54515e7da
Vinicius Tibo <viniciustibo.vt@gmail.com>
2023-09-17 17:17:28 -0300

    feat: create favorite page

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
+import CardLastNews from "../components/CardLastNews";
+import CardNews from "../components/CardNews";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchNews } from "../redux/actions";
+import { Dispatch, StoreType } from "../types";
+import NavBar from "../components/NavBar";
+
 function Favorite() {
+  const dispatch: Dispatch = useDispatch()
+  const { newsArray, favoriteNews, firstNews } = useSelector((state: StoreType) => state.news);
+  const FavoriteArray = newsArray.filter((news) => favoriteNews.includes(news.id));
+  const [numberOfNews, setNumberOfNews] = useState(9);
+
+  useEffect(() => {
+    dispatch(fetchNews());
+  }, []);
+  if (newsArray.length < 1) {
+    return <h1>Carregando...</h1>;
+  }
+
+  const handleShowNews = () => {
+    setNumberOfNews(numberOfNews + 3);
+  };
+
   return (
-    <div>
-      <h1>Favorite</h1>
-    </div>
+    <>
+      <div>
+        <CardLastNews lastNews={firstNews} />
+      </div>
+      <NavBar />
+      <div>
+        {FavoriteArray.slice(0, numberOfNews).map((news) =>
+          <div key={news.id}>
+            <CardNews news={news} />
+          </div>
+        )}
+      </div>
+      <button onClick={handleShowNews}>Carregar mais...</button>
+    </>
   );
 }
 
